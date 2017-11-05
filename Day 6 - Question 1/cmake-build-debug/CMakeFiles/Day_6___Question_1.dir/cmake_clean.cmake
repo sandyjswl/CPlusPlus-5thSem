6525718d2e46eb9a2c529f9c920c46a490a9d169
sandyjswl <sandeepjswl123@gmail.com>
2017-11-05 23:39:25 +0530

    First Commit

---
+++ Day 6 - Question 1/cmake-build-debug/CMakeFiles/Day_6___Question_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day_6___Question_1.dir/main.cpp.obj"
+  "Day_6___Question_1.pdb"
+  "Day_6___Question_1.exe"
+  "Day_6___Question_1.exe.manifest"
+  "libDay_6___Question_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day_6___Question_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Day 6 - Question 1/main.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    int val;
+public:
+    A();
+    A(int);
+    void show();
+    A operator+(A);
+    A operator+(int);
+    friend A operator +(int, A&);
+
+    A operator-(A);
+    A operator ++();
+    A operator ++(int);
+    friend A operator --(A &);
+
+};
+
+A operator+(int x,A&t)
+{
+    A r;
+    r.val=x+t.val;
+    return r;
+
+}
+
+
+A operator --(A &t)
+{
+    t.val--;
+    return t;
+}
+
+
+
+A:: A(){
+    val=0;
+}
+A:: A(int v){
+    val=v;
+}
+void A:: show(){
+    cout<<" "<<val<<endl;
+}
+A A:: operator+(A temp){
+    val=val+temp.val;
+    return *this;
+}
+
+A A:: operator+(int x){
+    val=val+x;
+    return *this;
+}
+
+
+
+
+
+A A:: operator-(A temp){
+    val=val-temp.val;
+    return *this;
+}
+
+A A:: operator++()
+{
+    val++;
+    return *this;
+}
+A A:: operator++(int x)
+{
+    val++;
+    return *this;
+}
+
+
+int main()
+{
+    A a(50),b(20),c(10),d;
+    cout<<"Value of A is:";
+    a.show();
+    cout<<"Value of B is:";
+    b.show();
+    cout<<"Value of C is:";
+    c.show();
+    cout<<"Result is :";
+    d=a+b-c;
+    d.show();
+
+}
